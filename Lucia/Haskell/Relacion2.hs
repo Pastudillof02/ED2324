5667fe6c51b95e69edb3fc2a96b1148b56fe724d
luciarosales <luciarosales@uma.es>
2023-10-02 10:30:24 +0200

    Update Relacion2

--- Lucia/Haskell/Relacion2.hs
+++ Lucia/Haskell/Relacion2.hs
@@ -30,19 +30,20 @@ sorted (x:y:zs) = x <= y && sorted(y:zs)
 
 
 --con acumuladores:
-sumatorio :: Integer -> Integer
+{-sumatorio :: Integer -> Integer
 sumatorio n = aux 0 necesario
     where
         | aux ac 0 = ac
-        | aux ac n | n>0 = aux (ac+n) (n-1)
-
+        | aux ac n 
+        | n>0 = aux (ac+n) (n-1)
+-}
 
 --longitud de una lista con parametros acumuladores
-long :: [a] -> Int
+{-long :: [a] -> Int
 long xs = aux 0 xs
     where
         | aux ac [] = ac
-        | axu ac (x:xs) = aux (ac+1) xs
+        | aux ac (x:xs) = aux (ac+1) xs
 
 
 toma :: Int -> [a] -> [a]
@@ -52,10 +53,94 @@ toma n (x:xs)
     | n>0 = x : toma (n-1) xs
     | otherwise = []
 
-
+-}
 --take 10(map (*10) [1,3..]) si coloco esto en la consola me coge 10 elementos de la lista infinita y me los multiplica cada uno por 10
 --take 10(filter (>100) (map (*10) [1,3..])) 
+
 
 --argumentos simples x y z
 --argumentos compuestos xs ys zs
 -- para definir una lista infinita [1,3..]
+
+{-
+Cada pareja son equivalencias
+[ x^2 | x <- [1..10]]
+map (^2) [1..10]
+
+[ x | x <- [1..10], x>5]
+filter (>5) [1..10]
+
+[ x^2 | x <- [1..10], x>5]
+map (^2) (filter (>5) [1..10])
+
+foldr (\x y -> x+y) 10 [1,2,3]  
+foldr add 10 [1,2,3]
+foldl (+) 10 [1,2,3] -- al menos en este caso/operacion en otras no es asi
+
+
+foldr (*) 1 [1,2,3,4]
+foldr (\x y -> x*y) 1 [1,2,3,4]
+
+foldr (:) [] [1,2,3,4] --añado elem, res [1,2,3,4]
+foldr (\x xs -> x :xs) [] [1,2,3,4] 
+
+foldr (\x xs -> xs ++ [x]) [] [1,2,3,4] --res [4,3,2,1] 
+foldl (\xs x -> x : xs) [] [1,2,3,4] -- res [4,3,2,1] mejor utilizar este en el caso de que tenga que invertir una lista pq es mas eficiente
+
+foldr (\x n -> n+1) 0 [1,2,3,4] calcula la longitud de la lista
+foldl (\n x -> n+1) 0 [1,2,3,4]
+
+foldr (\x xs -> if even x then x:xs else xs) [] [1,2,3,4] -- filtro y me quedo con los pares
+
+
+map (f 10 20) [1,2,3] -- es correcto debido a q es una funcion que espera un argumento para producir un resu
+
+map (map suma10) [[1,2,3], [4,5], [6,7,8]] --esto esta bien
+map (map (+10)) [[1,2,3], [4,5], [6,7,8]] --esto esta bien
+map (map suma10) [1,2,3] --esto estaria mal, porque espera una lista de sublistas
+
+-}
+
+isMultipleOf :: Integer -> Integer -> Bool
+isMultipleOf y x = mod x y ==0
+
+{- si pongo lo siguiente en la consola :t isMultipleOf 3
+isMultipleOf 3 :: Integer -> Bool esta es la funcion que me devuelve 
+con esa funcion puedo aplicarla en filter por ej: filter(isMultipleOf 2) [1..10] pq requeria una funcion con esa estructura-}
+
+
+esPar :: Integer -> Bool
+esPar x = mod x 2 == 0 
+
+misterio :: Integer -> Bool
+misterio = not . esPar
+
+-- Bool -> Bool     .     Integer -> Bool  =   Bool -> Bool 
+
+--pag 45
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
