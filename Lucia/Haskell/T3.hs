c35d1f7522dec537aa9e1038dd15bf06586599ba
luciarosales <luciarosales@uma.es>
2023-10-18 12:18:37 +0200

    Actualizacion de carpeta

--- Lucia/Haskell/T3.hs
+++
@@ -1,8 +0,0 @@
-import Tema3
-import Data.Char --necesario importarlo aunque este importado en el otro modulo
-
-f4 :: Integer -> Integer -> Integer
-f4 x y = even (f1 x y) 
-
-f5 :: Char -> Char
-f5 c = f3(f3 c) --devuelve el siguiente caracter
